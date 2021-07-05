cf9fd22c139d7a72f5d0b8161f581c74bd4970c9
Adharsh <81523460+adharshrj@users.noreply.github.com>
2021-07-05 16:29:23 +0530

    Update MyNewHashMap.java

--- WK2/D6/MyNewHashMap.java
+++ WK2/D6/MyNewHashMap.java
@@ -16,16 +16,19 @@ class MyOwnClass{
 public class MyNewHashMap {
     public static void main(String args[])
     {
-        Hashtable<Integer, MyOwnClass> ht = new Hashtable<>();
+        Hashtable<Integer,MyOwnClass> ht = new Hashtable<>();
         ht.put(1, new MyOwnClass("Ram", 25));
         ht.put(2, new MyOwnClass("Laxman", 23));
         ht.put(3, new MyOwnClass("Sita", 24));
         ht.put(4, new MyOwnClass("Shyam", 20));
-
-        Iterator<String> ite = ht.iterator();
-        while (ite.hasNext()){
-            System.out.println("The Hashtable is" + ht);
-        }
+    
+        Set<Integer> s = ht.keySet();
+     
+        Iterator<Integer> itr = s.iterator();
+     
+        while (itr.hasNext()) { 
+           System.out.println(ht);
+        } 
 
     }
 }
